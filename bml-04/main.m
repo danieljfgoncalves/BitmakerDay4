7ee940ede9729411b9df7ba08985f87a3f537bfe
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-09 10:54:44 -0400

    my tests

--- bml-04/main.m
+++ bml-04/main.m
@@ -8,10 +8,20 @@
 
 #import <Foundation/Foundation.h>
 
+
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        
+        NSDate* today = [NSDate date];
+        NSObject* object2 = [NSObject new];
+        
+        object2 = @[@"Bye", @"Xau", @"Ciau"];
+    
+        
+        NSLog(@"Hello, World! Today is %@. %@", today, object2);
     }
     return 0;
 }
